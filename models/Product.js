189680a280afbca68f12ff10b0e04ecb13570b82
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-05 16:00:48 -0400

    set up Product model in Product.js

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        //must be a decimal
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        //must be at least 10 characters long
+        len: [10],
+        //must be a number
+        isNumeric: true
+    }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
