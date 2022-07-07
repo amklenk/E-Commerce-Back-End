8ad3829180744e9b3a2ae34d71737d214690d515
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-07 19:14:08 -0400

    all routes now working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -61,7 +61,7 @@ router.post('/', (req, res) => {
   // "price": 200.00,
   //"stock": 3,
   //"category_id": 2
-  //"tagIds" : [productTagIds]
+  //"tagIds" : [1, 2, 3, 4]
   //
   /* req.body should look like this...
     {
@@ -95,7 +95,7 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
-  // update product data
+  // update product tags
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -110,7 +110,7 @@ router.put('/:id', (req, res) => {
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
+      .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
             product_id: req.params.id,
@@ -130,7 +130,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
